014e59d162d42da29889c290cc413aa26847a694
Diogo <dlsequeira75@gmail.com>
2020-12-06 01:55:11 +0000

    Changed: Item Class
    Added a addNewPrice() method to the class. It adds to the first element of product's priceHistory a new value if it's different to that current value in the first element and returns a boolean to check if the operation was successful

--- app/src/main/java/pt/isec/amov_tp/data/Item.kt
+++ app/src/main/java/pt/isec/amov_tp/data/Item.kt
@@ -4,12 +4,20 @@ import java.io.Serializable
 
 data class Item(var product: Product, var quantity : Quantity, var inCart : Boolean = false) : Serializable {
 
-
     override fun toString(): String {
         return "Item(category=$product.category, description='$product.description', brand='$product.brand', quantity='$quantity', priceHistory=${product.priceHistory}, notes='$product.notes')"
     }
 
     fun toStringSmall() : String {
         return product.description;
+    }
+
+    fun addNewPrice(newPrice: Double): Boolean{
+
+        if(newPrice != product.priceHistory[0]) {
+            product.priceHistory.add(0, newPrice)
+            return true
+        }
+        return false
     }
 }
