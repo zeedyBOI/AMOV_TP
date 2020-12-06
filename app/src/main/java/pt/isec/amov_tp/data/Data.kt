e36c6dbdb80d3eb119ae7de1076f3ef2dbbbf7c6
Diogo <dlsequeira75@gmail.com>
2020-12-06 01:24:57 +0000

    Changed: Data Classes
    Data: Added a Unit List to store Units
    ShoppingList: Added a getTotalPrice method

--- app/src/main/java/pt/isec/amov_tp/data/Data.kt
+++ app/src/main/java/pt/isec/amov_tp/data/Data.kt
@@ -2,8 +2,9 @@ package pt.isec.amov_tp.data
 
 import java.io.Serializable
 
-class Data(var productsList: ArrayList<Product>, var shopList: ArrayList<ShoppingList>, var categoryList: ArrayList<String>) : Serializable {
+class Data(var productsList: ArrayList<Product>, var shopList: ArrayList<ShoppingList>, var categoryList: ArrayList<String>, var unitList: ArrayList<String>) : Serializable {
     fun addList(list : ShoppingList) { shopList.add(list) }
     fun addProduct(product : Product) { productsList.add(product) }
-    fun addCategory(category : String) {categoryList.add(category) }
+    fun addCategory(category : String) { categoryList.add(category) }
+    fun addUnit(unit: String) { unitList.add(unit) }
 }
--- app/src/main/java/pt/isec/amov_tp/data/ShoppingList.kt
+++ app/src/main/java/pt/isec/amov_tp/data/ShoppingList.kt
@@ -3,18 +3,27 @@ package pt.isec.amov_tp.data
 import java.io.Serializable
 
 data class ShoppingList(var name : String, var listItems : ArrayList<Item>) : Serializable {
-    var totalPrice = "0.0"
+
     constructor(name: String, priceString: String, items: ArrayList<Item>) : this(name, items) {
         if (items.isNullOrEmpty())
             listItems = arrayListOf()
-        totalPrice = priceString
     }
 
     fun addItem(item : Item) : Boolean{
         return listItems.add(item)
     }
 
+    fun getTotalPrice() : Double{
+        var totalPrice : Double = 0.00
+        for(index in listItems) {
+            totalPrice += index.product.priceHistory[0]
+        }
+
+        return totalPrice
+    }
+
     override fun toString(): String {
         return "ShoppingList: $name, $listItems"
     }
+
 }
